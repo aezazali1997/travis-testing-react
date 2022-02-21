84d562103f9cf4e92889a0701f45c19e92d36419
aezazali1997 <aezazali.pro@gmail.com>
2022-02-21 13:45:03 +0500

    created a unit test case

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +14,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Learning unit tests</p>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('learning react testing ', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learning unit tests/i);
+  expect(linkElement).toBeInTheDocument();
+});
